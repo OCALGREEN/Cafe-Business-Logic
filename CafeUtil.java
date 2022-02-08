318f24cc60374f10fe6c2d5ca33ab9f0dc8b51d0
Oger Carrillo <ogercarrillo@gmail.com>
2022-02-08 13:00:05 -0800

    finished_with_bonuses

--- CafeUtil.java
+++ CafeUtil.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CafeUtil {
 
     public int getStreakGoal(int numWeeks) {
@@ -16,17 +18,50 @@ public class CafeUtil {
         return sum; 
     }
 
-
     public void displayMenu(ArrayList<String> menuItems) {
-
-        
+        for(int i = 0; i < menuItems.size(); i++) {
+            String item = menuItems.get(i); 
+            System.out.println(item); 
+        }
     }
 
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter you name: "); // asks the user for his name
+        String userName = System.console().readLine(); // logs the user's input
+        customers.add(userName);  // adds the user's name to the array
+        System.out.println("Hello " + userName); // greets the user
+        System.out.println("There are " + (customers.size() - 1) + " people infront of you."); // calculates the # of people infront of them
 
+        System.out.println("\n----- Customer List-----");
+        for(int i = 0; i < customers.size(); i++) { // ouputs all the users
+            String people = customers.get(i);
+            System.out.println(people); 
+        }
+    }
 
+    public void printPriceChart(String product, double price, int maxQuantity) {
+        System.out.println(product);
+        int count = 1; 
+        double priceSum = price; 
 
+        for(int i = 0; i < maxQuantity; i++) {
+            System.out.println(count+ " - " + priceSum);
+            count = count + 1; 
+            priceSum = priceSum + price; 
+        }
+    }
 
-
+    boolean displayMenu(ArrayList<String> menuItems, ArrayList<Double> prices) {
+        if (menuItems.size() != prices.size()) {
+            return false; 
+        }
+        for(int i = 0; i < menuItems.size(); i++) {
+            String item = menuItems.get(i); 
+            Double price = prices.get(i); 
+            System.out.println(i + " " + item + " -- " + price); 
+        }
+        return true; 
+    }
 
 
 }
--- TestCafe.java
+++ TestCafe.java
@@ -14,21 +14,31 @@ public class TestCafe {
         double[] lineItems = {3.5, 1.5, 4.0, 4.5};
         System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
         
-        // System.out.println("----- Display Menu Test-----");
-        
-        // ArrayList<String> menu = new ArrayList<String>();
-        // menu.add("drip coffee");
-        // menu.add("cappuccino");
-        // menu.add("latte");
-        // menu.add("mocha");
-        // appTest.displayMenu(menu);
+        System.out.println("----- Display Menu Test-----");
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        appTest.displayMenu(menu); 
     
-        System.out.println("\n----- Add Customer Test-----");
-        ArrayList<String> customers = new ArrayList<String>();
-        // --- Test 4 times ---
-        for (int i = 0; i < 4; i++) {
-            appTest.addCustomer(customers);
-            System.out.println("\n");
-        }
+        // System.out.println("\n----- Add Customer Test-----");
+        // ArrayList<String> customers = new ArrayList<String>();
+        // // --- Test 4 times ---
+        // for (int i = 0; i < 4; i++) {
+        //     appTest.addCustomer(customers);
+        //     System.out.println("\n");
+        // }
+
+        System.out.println("\n---- Price Chart ----");
+        appTest.printPriceChart("Colombia Coffee Grounds", 15, 3); 
+
+        System.out.println("\n---- Overloading Display Menu----");
+        ArrayList<Double> prices = new ArrayList<Double>(); 
+        prices.add(1.5);
+        prices.add(3.5);
+        prices.add(4.5);
+        prices.add(3.5);
+        appTest.displayMenu(menu, prices); 
     }
 }
